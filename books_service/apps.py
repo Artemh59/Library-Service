1a5a77f205d0b8c615e96f6607f19860f43b6204
Artem <hartem472@gmail.com>
2024-03-13 13:17:52 +0100

    initialized books_service app

--- books_service/apps.py
+++ books_service/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LibraryServiceApiConfig(AppConfig):
+class BooksServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'library_service_api'
+    name = 'books_service'
