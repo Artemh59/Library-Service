ad09db6b8959e4c4d3f9d6e7fb26b0f66427e485
Artem <hartem472@gmail.com>
2024-03-13 14:23:24 +0100

    created urls for book model

---
+++ books_service/urls.py
@@ -0,0 +1,8 @@
+from books_service.views import BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("books", BookViewSet, basename='book')
+urlpatterns = router.urls
+
+app_name = "books"
