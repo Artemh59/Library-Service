03d6488250a503c924ea7fc35765f4007d51e035
Artem <hartem472@gmail.com>
2024-03-15 12:51:51 +0100

    created 'me' endpoint

--- user/views.py
+++ user/views.py
@@ -5,3 +5,10 @@ from user.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class MeView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
