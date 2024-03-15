d6cce27995d8a77cd5ffc9efadbf833beb66b62a
Artem <hartem472@gmail.com>
2024-03-15 11:43:12 +0100

    added user to admin & AUTH_USER_MODEL

--- books_service/urls.py
+++ books_service/urls.py
@@ -2,7 +2,7 @@ from books_service.views import BookViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register("books", BookViewSet, basename='book')
+router.register("books", BookViewSet, basename="book")
 urlpatterns = router.urls
 
 app_name = "books"
