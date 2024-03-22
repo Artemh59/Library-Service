448082d65e933ae3fd0e9a73303d045b6aab8bc6
Artem <hartem472@gmail.com>
2024-03-22 17:37:04 +0100

    changed book model

--- books_service/models.py
+++ books_service/models.py
@@ -10,7 +10,7 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     cover = models.CharField(max_length=8, choices=COVER_CHOICES)
-    inventory = models.PositiveIntegerField()
+    inventory = models.IntegerField()
     daily_fee = models.DecimalField(decimal_places=2, max_digits=7)
 
     def __str__(self):
