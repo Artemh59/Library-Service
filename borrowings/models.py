278905761de68fbaf018839c9be5d81475f89e21
Artem <hartem472@gmail.com>
2024-03-22 12:42:56 +0100

    added validation to borrowing model & changed book model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
 from books_service.models import Book
 
@@ -10,6 +11,15 @@ class Borrowing(models.Model):
     actual_return_date = models.DateField()
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="borrowings")
+
+    def clean(self):
+        if not (self.book.inventory > 0):
+            raise ValidationError("book inventory must be more than 0")
+        self.book.inventory -= 1
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return f"book: {self.book}, user: {self.user}"
