bc3136160c021c1173cf4da732085277dde7c3e4
Artem <hartem472@gmail.com>
2024-04-02 18:07:51 +0200

    created async function for telegram notifications

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,10 +1,28 @@
 from rest_framework.generics import RetrieveAPIView, ListCreateAPIView, DestroyAPIView
 from rest_framework.response import Response
 from rest_framework import status
+from telethon.sync import TelegramClient
+import asyncio
 
 from borrowings.serializers import BorrowingListCreateSerializer, BorrowingDetailSerializer
 from borrowings.models import Borrowing
 from books_service.models import Book
+
+
+async def helper(message_text: str) -> None:
+    api_id = "27558711"
+    api_hash = "2b9adc6696c8b335eee6ea7f4516b059"
+    bot_token = "7149674498:AAHF9vFWSej_XGCsp3wj_MycLjmbXRAO5II"
+    group_id = -1001959625843
+
+    async with TelegramClient("bot_session", api_id, api_hash) as client:
+        await client.start(bot_token=bot_token)
+
+        await client.send_message(group_id, message_text)
+
+
+async def main(message_text: str) -> None:
+    await helper(message_text)
 
 
 class BorrowingListCreateView(ListCreateAPIView):
@@ -25,6 +43,8 @@ class BorrowingListCreateView(ListCreateAPIView):
 
         book.inventory -= 1
         book.save()
+
+        asyncio.run(main("book has borrowed"))
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
