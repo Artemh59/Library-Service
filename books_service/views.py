4c6bee65ed75d5bec3f9df778362c43b42ff54db
Artem <hartem472@gmail.com>
2024-04-08 10:55:53 +0200

    added default permission class

--- books_service/views.py
+++ books_service/views.py
@@ -2,10 +2,8 @@ from rest_framework import viewsets
 
 from books_service.serializers import BookSerializer
 from books_service.models import Book
-from books_service.permissions import IsAdminOrReadOnly
 
 
 class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
-    permission_classes = (IsAdminOrReadOnly,)
--- library_service/settings.py
+++ library_service/settings.py
@@ -138,6 +138,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "books_service.permissions.IsAdminOrReadOnly",
+    ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
