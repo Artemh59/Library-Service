80183a669b6d820fd07cbbe270e640177292badb
Artem <hartem472@gmail.com>
2024-04-10 22:05:41 +0200

    Added all logic related to the notification in separate module

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,36 +1,16 @@
 from rest_framework.generics import RetrieveAPIView, ListCreateAPIView, DestroyAPIView
 from rest_framework.response import Response
 from rest_framework import status
-from telethon.sync import TelegramClient
 import asyncio
-from dotenv import dotenv_values
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 
+from telegram_notifications.send_notifications import notification
 from borrowings.serializers import (
     BorrowingListCreateSerializer,
     BorrowingDetailSerializer,
 )
 from borrowings.models import Borrowing
 from books_service.models import Book
-
-ENV_VALUES = dotenv_values(".env")
-
-
-async def helper(message_text: str) -> None:
-    api_id = ENV_VALUES["API_ID"]
-    api_hash = ENV_VALUES["API_HASH"]
-    bot_token = ENV_VALUES["BOT_TOKEN"]
-    group_id = int(ENV_VALUES["GROUP_ID"])
-
-    async with TelegramClient("bot_session", api_id, api_hash) as client:
-        await client.start(bot_token=bot_token)
-
-        await client.send_message(group_id, message_text)
-
-
-async def notification(message_text: str) -> None:
-    await asyncio.sleep(0.1)
-    await helper(message_text)
 
 
 class BorrowingListCreateView(ListCreateAPIView):
